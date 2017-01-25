17db44ec69e8d1211d09dd2ac80c8251fab8fd1f
Diorgines Lacerda <diorgineslacerda@gmail.com>
2017-01-25 12:41:55 -0200

    Aula Final

--- index.js
+++ index.js
@@ -1,34 +1,80 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
-const fs = require('fs');
-const middleware = require('./log-middleware')
+const middleware = require('./log-middleware');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Desenvolvedores = require('./models/desenvolvedores');
+
+var options = {
+    server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+    replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }
+};
+
+mongoose.connect('mongodb://teste:teste123@ds127949.mlab.com:27949/workshop', options,
+    (err) => {
+        if (err) console.log('Não consegui conectar');
+        else console.log('Conectado com sucesso!');
+    });
+
+app.set('view engine', 'pug');
 
 app.use(middleware);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) =>{
-    fs.readFile('./views/index.html', 'UTF-8', (err, content) =>{
-        if (err) res.status(500).send('Ocorreu um erro');
-        res.send(content);
-    });
-    //res.send('Você chegou na minha aplicação!');
+app.get('/', (req, res) => {
+    res.render('index', { nomes: ['teste 1', 'teste 2', 'teste 3'] });
 });
 
-
-app.get('/contato', (req,res) =>{
-    fs.readFile('./views/contato.html', 'UTF-8', (err, content) =>{
-        if (err) res.status(500).send('Ocorreu um erro');
-        res.send(content);
-    });
+app.get('/contato', (req, res) => {
+    res.render('contato');
 });
 
-app.get('/sobre', (req,res) =>{
-    fs.readFile('./views/sobre.html', 'UTF-8', (err, content) =>{
-        if (err) res.status(500).send('Ocorreu um erro');
-        res.send(content);
-    });
+app.get('/sobre', (req, res) => {
+    res.render('sobre');
 });
 
+app.get('/cadastrar', (req, res) => {
+    res.render('cadastrar');
+});
 
-app.listen(3001, () =>{
-    console.log('Voce está rodando a aplicação na porta 3001');
+var indice = 0;
+app.route('/desenvolvedores')
+    .post((req, res) => {
+        var desenvolvedores = new Desenvolvedores({
+            nome: req.body.nome,
+            email: req.body.email,
+            indice: ++indice
+        });
+        desenvolvedores.save((err) => {
+            if (err) res.status(500).send('Ocorreu um erro');
+
+            Desenvolvedores.find({},
+                (err, desenvolvedores) => {
+                    if (err) res.status(500).send('Deu erro');
+
+                    res.render('desenvolvedores', { desenvolvedores: desenvolvedores });
+                });
+        });
+    })
+    .get((req, res) => {
+        Desenvolvedores.find({},
+            (err, desenvolvedores) => {
+                if (err) res.status(500).send('Deu erro');
+
+                res.render('desenvolvedores', { desenvolvedores: desenvolvedores });
+            });
+    });
+
+app.get('/sortear', (req, res) => {
+    var randomNumber = Math.floor((Math.random() * 8) + 1);
+
+    Desenvolvedores.findOne({ indice: randomNumber }, (err, dev) => {
+        res.render('sortear', { dev: dev });
+    });
+
+});
+
+app.listen(3001, () => {
+    console.log('Você está rodando a aplicação na porta 3001');
 });
--- log-middleware.js
+++ log-middleware.js
@@ -1,5 +1,4 @@
 module.exports = (req, res, next) => {
-    console.log("Passei pela requisição");
-    console.log("Requisição feita em " + req.url);
+    console.log('Requisição feita em ' + req.url);
     next();
 }
